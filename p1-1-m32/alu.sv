2d71154ed2a87c87bd657c7e5be997afa5b2e4ac
Sukhbir Singh <sukhbirs112@gmail.com>
2018-02-10 21:56:31 -0600

    forgot to add alu.sv before commiting. This is the fix.

--- p1-1-m32/alu.sv
+++ p1-1-m32/alu.sv
@@ -1,29 +1,52 @@
 
-module 2mux1_32bit(input wire [31:0] a, b,
-                   input wire s,
-                   output wire [31:0] q);
-  assign out = s ? a : b;
+module mux2to1( input wire [31:0] i0,i1, input wire op, output wire [31:0] out);
+assign out = op ? i1 : i0;
 endmodule
 
-module 4mux1_32bit(input wire [31:0] a, b, c, d,
-                   input wire [1:0] s,
-                   output wire [31:0] q);
-  ma 2mux1(a, b, s[0], qa);
-  mb 2mux1(c, d, s[0], qb);
-  assign q = s[1] ? qa : qb;
+module mux4to1 (input wire [31:0] i0,i1,i2,i3, input wire [1:0] op, output wire [31:0] out); 
+wire [31:0] outmux0, outmux1;
+mux2to1 dut0(i0,i1,op[0],outmux0);
+mux2to1 dut1(i2,i3,op[0],outmux1);
+mux2to1 dut2(outmux0,outmux1,op[1],out);
 endmodule
 
-module alu(input logic [31:0] a, b,
-           input logic [2:0] f,
-           output logic [31:0] y,
-           output logic zero);
-  assign begin
-    m1 2mux1_32bit(b, ~b, f[2], bb);
-    ma = a & bb;
-    mb = a | bb;
-    mc = a + bb;
-    md = a + bb; /* TODO: this should be zero-extend / SLT */
-    m 4mux1_32bit(ma, mb, mc, md, f[0:1], y);
-    zero = (y == 0) ? 1 : 0;
-  end
+
+module RCA (input wire [31:0] a,b, input wire op, output wire [31:0] s);
+
+//assign s = op ? A - B : A + B;
+//How to get Cout??? A + B is over 33 bits max , so we do the following
+assign s = op ? a + b + 1 : a + b;
+
+//op equal to 1 implies b is inverted
+
+// cout concatenated with s is over 33 bits
+
 endmodule
+
+
+module zeroextender(input wire a , output wire [31:0] aex);
+
+assign aex [0]= a;
+assign aex [31:1] = 30'b0;
+
+endmodule
+
+module ALU(input  logic [31:0] a, b, 
+	   input  logic [2:0]  f,
+	   output logic [31:0] y,output logic zero);
+
+wire [31:0]mux21out;
+wire [31:0]mux41out;
+wire [31:0]rcaout;
+wire cout;
+wire [31:0]zeroex;
+
+mux2to1 mux21(b,~b,f[2],mux21out);
+RCA adder(a,mux21out,f[2],rcaout,cout);
+zeroextender ze(rcaout[31],zeroex);
+mux4to1 mux41(a & mux21out, a | mux21out, rcaout,zeroex,f[1:0],mux41out);
+
+assign y = mux41out;
+assign zero = ~(|mux41out);
+
+endmodule 
