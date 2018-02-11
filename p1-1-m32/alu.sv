9109cfebd1ba775e7e699c12cdce8afc30ef3420
Gregory N. Schmit <schmitgreg@gmail.com>
2018-02-11 13:13:43 -0600

    cleanup, set relative path to test vector file

--- p1-1-m32/alu.sv
+++ p1-1-m32/alu.sv
@@ -14,35 +14,34 @@ module mux4to1_32bit(input wire [31:0] i0, i1, i2, i3,
   mux2to1_32bit m3(m1_out, m2_out, s[1], out);
 endmodule
 
-module RCA(input wire [31:0] a, b,
-           input wire op,
-           output wire [31:0] s);
+module adder_32bit(input wire [31:0] a, b,
+                    input wire op,
+                    output wire [31:0] y);
   //assign s = op ? A - B : A + B;
   //How to get Cout??? A + B is over 33 bits max , so we do the following
-  assign s = op ? a + b + 1 : a + b;
+  assign y = op ? a + b + 1 : a + b;
   //op equal to 1 implies b is inverted
   // cout concatenated with s is over 33 bits
 endmodule
 
-module zeroextender(input wire a, output wire [31:0] aex);
+module zero_extender(input wire a, output wire [31:0] aex);
   assign aex[0] = a;
   assign aex[31:1] = 30'b0;
 endmodule
 
-module ALU(input logic [31:0] a, b,
+module alu(input logic [31:0] a, b,
            input logic [2:0] f,
            output logic [31:0] y,
            output logic zero);
   wire [31:0] bb;
-  wire [31:0] mux41out;
-  wire [31:0] rcaout;
+  wire [31:0] s;
   wire cout;
   wire [31:0] zeroex;
 
   mux2to1_32bit bbmux(b, ~b, f[2], bb);
-  RCA adder(a, bb, f[2], rcaout);
-  zeroextender ze(rcaout[31], zeroex);
-  mux4to1_32bit ymux(a & mux21out, a | mux21out, rcaout, zeroex, f[1:0], y);
+  adder_32bit adder(a, bb, f[2], s);
+  zero_extender ze(s[31], zeroex);
+  mux4to1_32bit ymux(a & bb, a | bb, s, zeroex, f[1:0], y);
 
   assign zero = ~(|y);
 endmodule
--- p1-1-m32/alu_tb.sv
+++ p1-1-m32/alu_tb.sv
@@ -1,34 +1,33 @@
-`include "ALU.sv"
-module testALUecep1();
+`include "alu.sv"
+module alu_testbench();
 
-logic [31:0] a;
-logic [31:0] b;
-logic [3:0] f;
-logic [31:0] y;  //actual y value
-logic [31:0] expected_y; //expected y value
-logic z;
-logic [3:0]expected_z;//expected zero value
+  logic [31:0] a;
+  logic [31:0] b;
+  logic [3:0] f;
+  logic [31:0] y;  //actual y value
+  logic [31:0] expected_y; //expected y value
+  logic z;
+  logic [3:0]expected_z;//expected zero value
 
-//change first bracket numbers to reflect size of line
-//change second bracket numbers to reflect the number of lines
-logic [103:0] testvector[20:0];
-integer num; // used for iterating through testvector
-ALU dut(a,b,f[2:0],y,z); //please make sure I/Os are in correct order
+  //change first bracket numbers to reflect size of line
+  //change second bracket numbers to reflect the number of lines
+  logic [103:0] testvector[20:0];
+  integer num; // used for iterating through testvector
+  alu dut(a,b,f[2:0],y,z); //please make sure I/Os are in correct order
 
+  initial
+  begin
+  $readmemh("./alu.tv",testvector);
+  for (num = 0; num <21;num = num + 1)
+    begin
 
-initial
-begin
-$readmemh("C:/Modeltech_pe_edu_10.4a/examples/testvector.tv",testvector);
-for (num = 0; num <21;num = num + 1)
-begin
-
-{a,b,f,expected_y,expected_z} = testvector[num];#10;
-//use num + 1 since file lines start at 1
-$display("num=%d  a=%h b=%h f=%h",num+1,a,b,f);
-$display("exp_y=%h y=%h exp_z=%h z=%h",expected_y,y,expected_z,z);
-if (expected_y !=y | expected_z != z)
-	$display("Incorrect y or z line:%d---------------",num+1);
-end
-end
+    {a,b,f,expected_y,expected_z} = testvector[num];#10;
+    //use num + 1 since file lines start at 1
+    $display("num=%d  a=%h b=%h f=%h",num+1,a,b,f);
+    $display("exp_y=%h y=%h exp_z=%h z=%h",expected_y,y,expected_z,z);
+    if (expected_y !=y | expected_z != z)
+      $display("Incorrect y or z line:%d---------------",num+1);
+    end
+  end
 
 endmodule
