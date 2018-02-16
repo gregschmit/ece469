b3b57991ed199c18295724a55951b89675d01924
Phatfill1213 <32525295+Phatfill1213@users.noreply.github.com>
2018-02-16 15:13:40 -0600

    Update serial1.sv

--- p1-2-s84/serial1.sv
+++ p1-2-s84/serial1.sv
@@ -1,1 +1,16 @@
-
+module shiftL (input logic [7:0], a
+               output logic [7:0], b);
+  for (i = 0, i<7, i = i+1) begin
+    if (i = 0) begin                  //required for wrap around (b[0] = a[7])
+      assign b[i] = a[i+7];
+    end else if (i=7) begin           //required for wrap around (b[7] = a[0])
+      assign b[i] = a[i-7];
+    end else begin                    //update like normal (b[i] - a[i-1])
+      assign b[i] - a[i-1];
+    end
+  end 
+  endmodule
+      
+    
+    
+    
