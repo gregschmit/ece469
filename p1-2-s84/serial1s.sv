69a8b4ac3b3ff11aa3ac61f9932fb538a5080645
Gregory N. Schmit <schmitgreg@gmail.com>
2018-02-17 23:50:18 -0600

    bug fix bad structure

--- p1-2-s84/serial1s.sv
+++ p1-2-s84/serial1s.sv
@@ -2,9 +2,11 @@
 module serial1s(input logic [7:0] a,
                 output logic [3:0] y);
   int count = 0;
-  while (a != 0) begin
-    a = a & (a << 1);
-    count++;
+  initial begin
+    while (a != 0) begin
+      a = a & (a << 1);
+      count++;
+    end
+    assign y = count;
   end
-  assign y = count;
 endmodule
