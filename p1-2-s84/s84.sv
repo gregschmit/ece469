49899a511e58ecba352897f078f54d0c1517bcbd
Sukhbir Singh <sukhbirs112@gmail.com>
2018-02-18 12:58:11 -0600

    added 2 space indents

--- p1-2-s84/s84.sv
+++ p1-2-s84/s84.sv
@@ -14,16 +14,16 @@ module s84(input wire [7:0] A,
            output wire [7:0] Y,
            output wire [3:0] Z);
 
-wire [7:0] ca2Out;
-wire [7:0] sqmOut;
-wire [3:0] c1sOut;
+  wire [7:0] ca2Out;
+  wire [7:0] sqmOut;
+  wire [3:0] c1sOut;
 
-ca2 ca2Device(A,B,ca2Out);
-sqm sqmDevice(A, B, f_loc, f_type, Cout, sqmOut);
-//serial1s c1sDevice(Y,c1sOut);
-assign Y = op ? ca2Out : sqmOut;
-assign Z = 0; //default until counter works
-//assign Z = c1sOut;
+  ca2 ca2Device(A,B,ca2Out);
+  sqm sqmDevice(A, B, f_loc, f_type, Cout, sqmOut);
+  //serial1s c1sDevice(Y,c1sOut);
+  assign Y = op ? ca2Out : sqmOut;
+  assign Z = 0; //default until counter works
+  //assign Z = c1sOut;
 endmodule
 
 
@@ -37,56 +37,58 @@ module sqm(input wire [7:0] A,
            output wire [7:0] Cout,
            output wire [7:0] out);
 
-//f_loc goes into decoder that produces 8bit string with only one 1
-//each bit of C(=Bsqrd) goes into 4to1 mux e.g. (C,0,1,C_not) controlled by f_type
-//this 4to1 mux output goes into 2to1 mux containing (C,4to1muxout) controlled by the f_loc_decoder output
+  //f_loc goes into decoder that produces 8bit string with only one 1
+  //each bit of C(=Bsqrd) goes into 4to1 mux e.g. (C,0,1,C_not) 
+  //controlled by f_type
+  //this 4to1 mux output goes into 2to1 mux containing (C,4to1muxout) 
+  //controlled by the f_loc_decoder output
 
-wire [7:0] loc;//output of a decoder, used to determine which bit to change.
-wire [7:0] m41out; //output of 8 separate 4to1 muxes
-wire [7:0] C; // intermediate, equal to B*B
-wire [7:0] m21out;// output of 8 separate 2to1 muxes
+  wire [7:0] loc;//output of a decoder, used to determine which bit to change.
+  wire [7:0] m41out; //output of 8 separate 4to1 muxes
+  wire [7:0] C; // intermediate, equal to B*B
+  wire [7:0] m21out;// output of 8 separate 2to1 muxes
 
-square multB(B,C);
-decoder dec_loc(f_loc,loc);
+  square multB(B,C);
+  decoder dec_loc(f_loc,loc);
 
-mux4to1_1bit m41_0(C[0],1'b0,1'b1,~C[0],f_type,m41out[0]);
-mux4to1_1bit m41_1(C[1],1'b0,1'b1,~C[1],f_type,m41out[1]);
-mux4to1_1bit m41_2(C[2],1'b0,1'b1,~C[2],f_type,m41out[2]);
-mux4to1_1bit m41_3(C[3],1'b0,1'b1,~C[3],f_type,m41out[3]);
-mux4to1_1bit m41_4(C[4],1'b0,1'b1,~C[4],f_type,m41out[4]);
-mux4to1_1bit m41_5(C[5],1'b0,1'b1,~C[5],f_type,m41out[5]);
-mux4to1_1bit m41_6(C[6],1'b0,1'b1,~C[6],f_type,m41out[6]);
-mux4to1_1bit m41_7(C[7],1'b0,1'b1,~C[7],f_type,m41out[7]);
+  mux4to1_1bit m41_0(C[0],1'b0,1'b1,~C[0],f_type,m41out[0]);
+  mux4to1_1bit m41_1(C[1],1'b0,1'b1,~C[1],f_type,m41out[1]);
+  mux4to1_1bit m41_2(C[2],1'b0,1'b1,~C[2],f_type,m41out[2]);
+  mux4to1_1bit m41_3(C[3],1'b0,1'b1,~C[3],f_type,m41out[3]);
+  mux4to1_1bit m41_4(C[4],1'b0,1'b1,~C[4],f_type,m41out[4]);
+  mux4to1_1bit m41_5(C[5],1'b0,1'b1,~C[5],f_type,m41out[5]);
+  mux4to1_1bit m41_6(C[6],1'b0,1'b1,~C[6],f_type,m41out[6]);
+  mux4to1_1bit m41_7(C[7],1'b0,1'b1,~C[7],f_type,m41out[7]);
 
-mux2to1_1bit m21_0(C[0],m41out[0],loc[0],m21out[0]);
-mux2to1_1bit m21_1(C[1],m41out[1],loc[1],m21out[1]);
-mux2to1_1bit m21_2(C[2],m41out[2],loc[2],m21out[2]);
-mux2to1_1bit m21_3(C[3],m41out[3],loc[3],m21out[3]);
-mux2to1_1bit m21_4(C[4],m41out[4],loc[4],m21out[4]);
-mux2to1_1bit m21_5(C[5],m41out[5],loc[5],m21out[5]);
-mux2to1_1bit m21_6(C[6],m41out[6],loc[6],m21out[6]);
-mux2to1_1bit m21_7(C[7],m41out[7],loc[7],m21out[7]);
+  mux2to1_1bit m21_0(C[0],m41out[0],loc[0],m21out[0]);
+  mux2to1_1bit m21_1(C[1],m41out[1],loc[1],m21out[1]);
+  mux2to1_1bit m21_2(C[2],m41out[2],loc[2],m21out[2]);
+  mux2to1_1bit m21_3(C[3],m41out[3],loc[3],m21out[3]);
+  mux2to1_1bit m21_4(C[4],m41out[4],loc[4],m21out[4]);
+  mux2to1_1bit m21_5(C[5],m41out[5],loc[5],m21out[5]);
+  mux2to1_1bit m21_6(C[6],m41out[6],loc[6],m21out[6]);
+  mux2to1_1bit m21_7(C[7],m41out[7],loc[7],m21out[7]);
 
-assign Cout = m21out;
-assign out = m21out % A;
+  assign Cout = m21out;
+  assign out = m21out % A;
 
 endmodule
 
 
 
 module square(input wire [3:0] B, output wire [7:0] C);
-assign C = B*B;
+  assign C = B*B;
 endmodule
 
 
 module decoder(input wire [2:0] loc, output wire [7:0] out);
 
-assign out[0] = ~loc[0] & ~loc[1] & ~loc[2];
-assign out[1] = ~loc[0] & ~loc[1] & loc[2];
-assign out[2] = ~loc[0] & loc[1] & ~loc[2];
-assign out[3] = ~loc[0] & loc[1] & loc[2];
-assign out[4] = loc[0] & ~loc[1] & ~loc[2];
-assign out[5] = loc[0] & ~loc[1] & loc[2];
-assign out[6] = loc[0] & loc[1] & ~loc[2];
-assign out[7] = loc[0] & loc[1] & loc[2];
+  assign out[0] = ~loc[0] & ~loc[1] & ~loc[2];
+  assign out[1] = ~loc[0] & ~loc[1] & loc[2];
+  assign out[2] = ~loc[0] & loc[1] & ~loc[2];
+  assign out[3] = ~loc[0] & loc[1] & loc[2];
+  assign out[4] = loc[0] & ~loc[1] & ~loc[2];
+  assign out[5] = loc[0] & ~loc[1] & loc[2];
+  assign out[6] = loc[0] & loc[1] & ~loc[2];
+  assign out[7] = loc[0] & loc[1] & loc[2];
 endmodule
--- p1-2-s84/s84_tb.sv
+++ p1-2-s84/s84_tb.sv
@@ -3,46 +3,47 @@ module s84_tb();
 
 
 
-logic [7:0] A;
-logic [3:0] B;
-logic op;
-logic [2:0] f_loc;
-logic [1:0] f_type;
-logic [7:0] C,C_expected;
-logic [7:0] Y, Y_expected;
-logic [3:0] Z, Z_expected;
-logic [32:0] testvector [4:0];
+  logic [7:0] A;
+  logic [3:0] B;
+  logic op;
+  logic [2:0] f_loc;
+  logic [1:0] f_type;
+  logic [7:0] C,C_expected;
+  logic [7:0] Y, Y_expected;
+  logic [3:0] Z, Z_expected;
+  logic [32:0] testvector [4:0];
 
 
-s84 s84alu(A, B, op, f_loc, f_type, C, Y, Z);
+  s84 s84alu(A, B, op, f_loc, f_type, C, Y, Z);
 
-integer num;
+  integer num;
 
-initial
-begin
+  initial
+  begin
 
-$readmemb("./s84.tv",testvector);
+  $readmemb("./s84.tv",testvector);
 
-op=0;//use sqm, not ca2
+  op=0;//use sqm, not ca2
 
-for (num = 0; num<5; num = num + 1)
-begin
-{A, B, f_loc, f_type, C_expected, Y_expected} = testvector[num]; #10;
-$display("num=%d",num+1);
-$display("A=%b B=%b f_loc=%b f_type=%b C_exp=%b Y_exp=%d", A, B, f_loc, f_type, C_expected, Y_expected);
-$display("C=%b Y=%d",C,Y);
-if (C != C_expected)
-begin
-$display("C does not equal C_exp line num=%d",num+1);
-end
-if (Y != Y_expected)
-begin
-$display("Y does not equal Y_exp line num=%d",num+1);
-end
+  for (num = 0; num<5; num = num + 1)
+  begin
+  {A, B, f_loc, f_type, C_expected, Y_expected} = testvector[num]; #10;
+  $display("num=%d",num+1);
+  $display("A=%b B=%b f_loc=%b f_type=%b C_exp=%b Y_exp=%d", A, B, f_loc, f_type, C_expected, 
+  Y_expected);
+  $display("C=%b Y=%d",C,Y);
+  if (C != C_expected)
+  begin
+  $display("C does not equal C_exp line num=%d",num+1);
+  end
+  if (Y != Y_expected)
+  begin
+  $display("Y does not equal Y_exp line num=%d",num+1);
+  end
 
-end//end for
+  end//end for
 
 
-end
+  end
 
 endmodule
