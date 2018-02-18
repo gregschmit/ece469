7175bc4027122a14ae6e59f16b3d7053ac90224f
Gregory N. Schmit <schmitgreg@gmail.com>
2018-02-18 00:32:40 -0600

    restructure serial1s to make synthesizable

--- p1-2-s84/serial1s.sv
+++ p1-2-s84/serial1s.sv
@@ -1,13 +1,36 @@
 
 module serial1s(input logic [7:0] a,
                 output logic [3:0] y);
-  int count = 0;
-  logic [7:0] b = a;
-  initial begin
-    while (b != 0) begin
-      b = b & (b << 1);
-      count++;
-    end
-    assign y = count;
+  int count;
+  int found;
+  logic [7:0] im[7];
+  always_comb begin
+    im[0] = a & (a << 1);
+    im[1] = im[0] & (im[0] << 1);
+    im[2] = im[1] & (im[1] << 1);
+    im[3] = im[2] & (im[2] << 1);
+    im[4] = im[3] & (im[3] << 1);
+    im[5] = im[4] & (im[4] << 1);
+    im[6] = im[5] & (im[5] << 1);
+
+    if (a == 0)
+      y = 0;
+    else if (im[0] == 0)
+      y = 1;
+    else if (im[1] == 0)
+      y = 2;
+    else if (im[2] == 0)
+      y = 3;
+    else if (im[3] == 0)
+      y = 4;
+    else if (im[4] == 0)
+      y = 5;
+    else if (im[5] == 0)
+      y = 6;
+    else if (im[6] == 0)
+      y = 7;
+    else
+      y = 8;
   end
+
 endmodule
