eef12a978a2a15e16965c72a67f849f939073bc2
Gregory N. Schmit <schmitgreg@gmail.com>
2018-04-22 17:46:25 -0500

    add actual mips testbench

--- p3-1/mips_tb.sv
+++ p3-1/mips_tb.sv
@@ -33,3 +33,36 @@ module top(input logic clk, reset,
  // memory
  mem mem(clk, memwrite, adr, writedata, readdata);
 endmodule
+
+module mips_tb();
+  logic clk;
+  logic reset;
+  logic [31:0] writedata, dataadr;
+  logic memwrite;
+
+  // instantiate device to be tested
+  top dut (clk, reset, writedata, dataadr, memwrite);
+
+  // initialize test
+  initial begin
+    reset <= 1; # 22; reset <= 0;
+  end
+
+  // generate clock to sequence tests
+  always begin
+    clk <= 1; # 5; clk <= 0; # 5;
+  end
+
+  // check results
+  always @(negedge clk) begin
+    if (memwrite) begin
+      if (dataadr===84 && writedata===15) begin
+        $display("Simulation succeeded");
+        $stop;
+      end else if (dataadr !== 80) begin
+        $display("Simulation failed");
+        $stop;
+      end
+    end
+  end
+endmodule
